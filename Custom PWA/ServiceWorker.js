d711298e3879c1dba6a987a174ee88bed0f56257
crayjake <mm18j2c@leeds.ac.uk>
2022-07-08 13:40:59 +0100

    Added new builds and some PWA builds

---
+++ Custom PWA/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "henryXjake-BallzXtreme-0.3";
+const contentToCache = [
+    "Build/Custom PWA.loader.js",
+    "Build/Custom PWA.framework.js.unityweb",
+    "Build/Custom PWA.data.unityweb",
+    "Build/Custom PWA.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
